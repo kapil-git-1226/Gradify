cae1d0753345dcf7b60fa35568f04605bda7ac6b
technolife1kkmm <141816199+technolife1kkmm@users.noreply.github.com>
2024-12-01 19:22:44 +0530

    ejs functionality to html

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from "express"
 import cors from "cors"
 import path from "path"
+import { fileURLToPath } from 'url';
 const app = express()
 app.set('view engine','ejs')
 app.use (cors({
     origin: "*",
     credentials: true
 }))
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
@@ -17,7 +20,31 @@ app.use("/api/pyqs",pyqsrouter)
 import userroute from "./routes/user.router.js"
 app.use("/api/users",userroute)
 export {app}
-
+app.set('views', path.join(__dirname, 'views'));
+app.use('/notes', express.static(path.join(__dirname, 'notes')));
+app.use('/lectures', express.static(path.join(__dirname, 'lectures')));
+app.use('/about', express.static(path.join(__dirname, 'about')));
+app.use('/Grade_predictor', express.static(path.join(__dirname, 'Grade_predictor')));
+app.use('/Groups', express.static(path.join(__dirname, 'Groups')));
+app.use('/pyqs', express.static(path.join(__dirname, 'pyqs')));
 app.get('/dashboard', (req, res) => {
   res.render('dashboard');
+});
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'notes', 'Gradify - Notes.html'));
+});
+app.get('/lectures', (req, res) => {
+  res.sendFile(path.join(__dirname, 'lectures', 'lecture-interface.html'));
+});
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'about', 'about-page.html'));
+});
+app.get('/Grade_predictor', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Grade_predictor', 'grade_predictor.html'));
+});
+app.get('/Groups', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Groups', 'group-page.html'));
+});
+app.get('/pyqs', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pyqs', 'pyqs.html'));
 });
